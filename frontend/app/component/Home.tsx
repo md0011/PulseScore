c1c939a8737723e300345b60d177e71fca4c3111
Mayur Devle <mayurd0303@gmail.com>
2024-09-09 11:28:13 +0000

    update UI

--- frontend/app/component/Home.tsx
+++ frontend/app/component/Home.tsx
@@ -150,7 +150,7 @@ function Home() {
               />
             </div>
           </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-4/6">
             <img
               className="object-cover object-center rounded"
               alt="logo"
