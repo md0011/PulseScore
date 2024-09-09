1477d489f2b136e7cb229e00affe1a52fe3ec359
Mayur Devle <mayurd0303@gmail.com>
2024-09-09 11:48:51 +0000

    make responsive

--- frontend/app/component/Home.tsx
+++ frontend/app/component/Home.tsx
@@ -128,8 +128,8 @@ function Home() {
   const unloggedInView = (
     <>
       <section className="text-gray-400 bg-gray-900 body-font flex items-center">
-        <div className="container mx-auto flex px-5 py-16 lg:py-24 md:flex-row flex-col items-center h-screen">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="container mx-auto flex px-5 py-16 lg:py-24 flex-wrap-reverse justify-center items-center h-screen">
+          <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
               Decentralized Reputation at Your Fingertips
             </h1>
@@ -150,7 +150,7 @@ function Home() {
               />
             </div>
           </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-4/6">
+          <div className="md:w-96 w-80">
             <img
               className="object-cover object-center rounded"
               alt="logo"
